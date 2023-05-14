60b62c4afbd81cfe7c7c71236afb2fba440e05ac
Mateusz K <mateusz.kierman@gmail.com>
2023-05-14 21:37:40 +0200

    ekran szczegółów notatki - z poradnika

--- app/src/main/java/com/example/notepad/composable/notes/NoteDetails.kt
+++ app/src/main/java/com/example/notepad/composable/notes/NoteDetails.kt
@@ -1,10 +1,15 @@
 package com.example.notepad.composable.notes
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
@@ -15,13 +20,21 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.notepad.AppBar
 import com.example.notepad.Constants
 import com.example.notepad.Constants.noteDetailPlaceHolder
 import com.example.notepad.R
+import com.example.notepad.ui.theme.NotepadTheme
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NoteDetails(noteId: Int, navController: NavController, viewModel: NotesViewModel) {
     val scope = rememberCoroutineScope()
@@ -36,4 +49,43 @@ fun NoteDetails(noteId: Int, navController: NavController, viewModel: NotesViewM
         }
     }
 
+    NotepadTheme {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            Scaffold(
+                topBar = {
+                    AppBar(
+                        title = note.value.title,
+                        onIconClick = {
+                            navController.navigate(Constants.noteEditNavigation(note.value.id))
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(R.drawable.edit_note),
+                                contentDescription = stringResource(R.string.note_edit),
+                                tint = Color.Black,
+                            )
+                        },
+                        iconState = remember { mutableStateOf(true) }
+                    )
+                },
+
+                ) {
+                Column(
+                    Modifier
+                        .fillMaxSize()
+                ) {
+
+                    Text(
+                        text = note.value.title,
+                        modifier = Modifier.padding(top = 24.dp, start = 12.dp, end = 24.dp),
+                        fontSize = 36.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(text = note.value.dateUpdated, Modifier.padding(12.dp), color = Color.Gray)
+                    Text(text = note.value.note, Modifier.padding(12.dp))
+                }
+
+            }
+        }
+    }
 }
