c9c50a91bc217476cac04dde481656b29d9638a3
Mateusz K <mateusz.kierman@gmail.com>
2023-05-27 17:38:41 +0200

    zmiany kosmetyczne

--- app/src/main/java/com/example/notepad/ui/theme/Color.kt
+++ app/src/main/java/com/example/notepad/ui/theme/Color.kt
@@ -2,11 +2,5 @@ package com.example.notepad.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 val textWhiteColor = Color(0xFFFFFFFF)
-
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 val primaryColor = Color(0xFFFFFFFF)
